aff2dc3345b04602a2c9475537f7b7ea877eaac4
Elisabeth Pring <710wonderdev@gmail.com>
2022-01-13 22:44:57 -0800

    Door Trigger Timer

--- OpenDoor.cpp
+++ OpenDoor.cpp
@@ -42,19 +42,37 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 	if (PressurePlates && PressurePlates->IsOverlappingActor(ActorThatOpens))
 	{
 		OpenDoor(DeltaTime);
+		DoorLastOpened = GetWorld()->GetTimeSeconds();
+	}
+	else
+	{
+		//if door has been open longer than DoorClosedDelay
+		if (GetWorld()->GetTimeSeconds() - DoorLastOpened > DoorClosedDelay)
+		{
+			ClosedDoor(DeltaTime);
+		}
+		
+
 	}
 
 }
 
 void UOpenDoor::OpenDoor(float DeltaTime)
 {
-	// UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
-	// UE_LOG(LogTemp, Warning, TEXT("Yaw is: %f"), GetOwner()->GetActorRotation().Yaw);
-
 	CurrentYaw = FMath::FInterpTo(CurrentYaw, TargetYaw, DeltaTime, 2.f);
 	FRotator DoorRotation = GetOwner()->GetActorRotation();
 	DoorRotation.Yaw = CurrentYaw;
 	GetOwner()->SetActorRotation(DoorRotation); 
 	
 }
+
+void UOpenDoor::ClosedDoor(float DeltaTime)
+{
+	CurrentYaw = FMath::FInterpTo(CurrentYaw, InitialYaw, DeltaTime, 4.f);
+	FRotator DoorRotation = GetOwner()->GetActorRotation();
+	DoorRotation.Yaw = CurrentYaw;
+	GetOwner()->SetActorRotation(DoorRotation); 
+	
+}
+
 
--- OpenDoor.h
+++ OpenDoor.h
@@ -25,6 +25,7 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 	void OpenDoor(float DeltaTime);
+	void ClosedDoor(float DeltaTime);
 
 	private:
 
@@ -39,6 +40,9 @@ public:
 
 	UPROPERTY(EditAnywhere)
 	AActor* ActorThatOpens;
+
+	float DoorLastOpened = 0.f;
+	float DoorClosedDelay = .7f;
 
 		 
 };
