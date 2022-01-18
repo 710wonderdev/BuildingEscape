a50ca9f56937b1b44f18bf932b3dcf8584a4e6e0
Elisabeth Pring <710wonderdev@gmail.com>
2022-01-17 23:07:30 -0800

    Getting Player Viewpoint

--- Grabber.cpp
+++ Grabber.cpp
@@ -1,7 +1,10 @@
 // Copyright Elisabeth Pring 2022
 
-
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
 #include "Grabber.h"
+
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -19,7 +22,7 @@ void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
 	
 }
 
@@ -29,6 +32,24 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Get player viewpoint
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint
+		(OUT PlayerViewPointLocation, 
+		OUT PlayerViewPointRotation
+		);
+
+	UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"), 
+		*PlayerViewPointLocation.ToString(), 
+		*PlayerViewPointRotation.ToString()
+		);
+
+	// logging out to test
+	
+	// ray-cast out to a certain distance (reach)
+
+	// see what it hits
 }
 
